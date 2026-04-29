f7269f3cf43074b673ba1546f3d3065ba65e56a1
Justin0477 <132772383+takeru006@users.noreply.github.com>
2026-04-29 14:25:20 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
-import express from 'express'
+import express from 'express';
 import marketRoutes from './routes/market.routes.js'
-
 const app=express()
-
 // Middleware para procesar JSON
 app.use(express.json());
-
-// Middleware para procesar datos de formularios (opcional, si usas x-www-form-urlencoded)
+// Middleware para procesar datos de formularios (opcional, si usas x-www-formurlencoded)
 app.use(express.urlencoded({ extended: true }));
-
 app.use(marketRoutes)
-
-app.use((req,res,next)=>{
-    res.status(404).json({
-        message:'Endpoint No encontrado'
-    })
-})
-
+app.use((req, res, next) => {
+ res.status(404).json({
+ message: "Favor realizar pruebas en los siguientes endpoints:",
+ endpoints: [
+ "https://URL GENERADA Railway /usuarios",
+ "https:// URL GENERADA Railway /productos"
+ ]
+ });
+});
 export default app;
