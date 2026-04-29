2eeb3327696b0495c9b18220c1ec2dcc8d62c2db
Justin0477 <132772383+takeru006@users.noreply.github.com>
2026-04-29 14:24:50 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import{config} from 'dotenv'
 config()
 
 export const PORT= process.env.PORT || 3000
-export const DB_HOST=process.env.DB_HOST || 'localhost'
+export const DB_HOST=process.env.DB_HOST || 'mysql.railway.internal'
 export const DB_PORT=process.env.DB_PORT || 3306
 export const DB_USER=process.env.DB_USER || 'root'
 export const DB_PASSWORD=process.env.DB_PASSWORD || ''
-export const DB_DATABASE=process.env.DB_DATABASE || 'market'
+export const DB_DATABASE=process.env.DB_DATABASE || 'railway'
